36a1b3c3883638055710057f74f398e8ac2e809a
Eduardo Pedroso <andreeduardopp@gmail.com>
2023-09-28 07:28:52 -0300

    rr

--- Eventos/tests.py
+++ Eventos/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from model_bakery import baker
+from rest_framework.test import APIClient ,APIRequestFactory
+from Usuarios.models import User 
+from datetime import date
+from .models import Categoria, Evento  
+from Eventos.views import EventoView
 
-# Create your tests here.
+class EventoViewTestCase(TestCase):
+    def setUp(self):
+
+        self.categoria = baker.make(Categoria)
+        self.user = baker.make(User)  
+        self.user2 = baker.make(User)
+        self.evento_com_user = baker.make(
+            Evento,
+            user=self.user,
+            data=date.today(),
+            descricao="Descrição do evento com usuário",
+            valor_entrada=10.00,
+            categoria=self.categoria,
+        )
+
+        # Crie outro evento não associado a nenhum usuário
+        self.evento_sem_user = baker.make(
+            Evento,
+            user=None,
+            data=date.today(),
+            descricao="Descrição do evento sem usuário",
+        )
+
+        self.evento_user2 = baker.make(
+            Evento,
+            user=self.user2,
+            data=date.today(),
+            descricao="Descrição do evento usuário2",
+        )
+
+    def test_get_events_by_user(self):
+        factory = APIRequestFactory()
+        request = factory.get('/user/eventos/')
+        request.query_params = {"user_id": self.user.id}
+        response = EventoView().get_events_by_user(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+ 
--- utils/utils.py
+++ utils/utils.py
@@ -2,49 +2,11 @@ from django.core.exceptions import ValidationError
 
 
 def valida_telefone(phone_number):
-    """
-    Validação de número de celular simples.
-    """
-    if len(phone_number) < 10:
+   
+    if len(phone_number) < 9:
         raise ValidationError('Número de celular muito curto')
-    elif len(phone_number) > 15:
+    elif len(phone_number) > 12:
         raise ValidationError('Número de celular muito longo')
 
 
-def _retorna_digitos(texto):
-    if not texto or not isinstance(texto, str):
-        return None
-    return ''.join([c for c in texto if c.isdigit()])
-
-def valida_cpf(cpf):
-    cpf = _retorna_digitos(cpf)
-    if not cpf or len(cpf) != 11:
-        return False
-
-    # Primeiro dígito
-    relacao_digito_peso = zip(
-        [int(digito) for digito in cpf[0:-2]], [peso for peso in range(10, 1, -1)])
-    resto_soma = sum(
-        [digito * peso for digito, peso in relacao_digito_peso]) % 11
-    digito_esperado = 11 - resto_soma if resto_soma >= 2 else 0
-    if cpf[-2] != str(digito_esperado):
-        return False
-
-    # Segundo dígito
-    relacao_digito_peso = zip(
-        [int(digito) for digito in cpf[0:-1]], [peso for peso in range(11, 1, -1)])
-    resto_soma = sum(
-        [digito * peso for digito, peso in relacao_digito_peso]) % 11
-    digito_esperado = 11 - resto_soma if resto_soma >= 2 else 0
-    if cpf[-1] != str(digito_esperado):
-        return False
-
-    return True
-
-
-def mascara_cpf(cpf):
-    if valida_cpf(cpf):
-        cpf = _retorna_digitos(cpf)
-        return f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}'
-    return None
 
